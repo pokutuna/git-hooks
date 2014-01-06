cbaed247bc8bcb270c69fc0daad218c6bc34de70
pokutuna <popopopopokutuna@gmail.com>
2014-01-06 15:11:46 +0900

    何も add されてない時は確認挟まない

--- pre-commit/confirm-special-branch.rb
+++ pre-commit/confirm-special-branch.rb
@@ -4,6 +4,10 @@
 # 確認するブランチ名
 SPECIAL_BRANCHES = %w( master staging )
 
+# 何も add されてない時は素通りさせる
+exit(true) if `git diff --name-only --cached`.empty?
+
+
 current_branch = `git rev-parse --abbrev-ref HEAD`.chomp
 
 if SPECIAL_BRANCHES.include?(current_branch)
