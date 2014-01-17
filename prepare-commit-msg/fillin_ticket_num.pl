386c8d6026957b09874ac5dfe2eaaa29097d1764
pokutuna <popopopopokutuna@gmail.com>
2014-01-17 22:11:04 +0900

    Path::Class への依存なくす

--- prepare-commit-msg/fillin_ticket_num.pl
+++ prepare-commit-msg/fillin_ticket_num.pl
@@ -1,5 +1,4 @@
-#! /usr/bin/env perl
-use Path::Class qw(file);
+#!/usr/bin/env perl
 
 my $ticket = do {
     `git symbolic-ref --short HEAD` =~ /^(\d+)/;
@@ -7,8 +6,12 @@ my $ticket = do {
 };
 
 if ($ARGV[1] eq '' && $ticket) {
-    my $prepared_msg = file($ARGV[0])->slurp;
-    my $writer = file($ARGV[0])->open('w') or die $!;
-    $writer->print("(refs #$ticket) ", $prepared_msg);
-    $writer->close;
+    open(my $fh, '+<', $ARGV[0]) or die $!;
+    my @message = <$fh>;
+    unshift @message, "(refs #$ticket) ";
+    seek $fh, 0, 0;
+    print $fh @message;
+    close $fh;
 }
+
+exit 0;
